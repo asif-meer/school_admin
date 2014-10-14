4ff060497bd8895e2e71199d9bf1c21f64948431
one <one@test.com>
2014-10-14 18:09:12 +0500

    fee category model added

---
+++ app/models/fees_category.rb
@@ -0,0 +1,2 @@
+class FeesCategory < ActiveRecord::Base
+end
---
+++ spec/models/fees_category_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe FeesCategory, :type => :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
