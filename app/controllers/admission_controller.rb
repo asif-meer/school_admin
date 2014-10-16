2cebbf38c84e7bb8b22facae1b209dab4756bd0f
Daniyal Rao <raothegreat_dani_91@hotmail.com>
2014-10-16 15:31:10 +0500

    routes changes

--- app/controllers/admission_controller.rb
+++ app/controllers/admission_controller.rb
@@ -1,49 +1,20 @@
 class AdmissionController < ApplicationController
   before_filter :authenticate_user!
-  def index
-    @students = Student.all
-  end
 
   def new
     @student = Student.new
     2.times { @student.emergency_contacts.build }
   end
 
-  def edit
-    @student = Student.find(params[:id])
-  end
-
-  def show
-    @student = Student.find(params[:id])
-  end
-
   def create
     @student = Student.new(admission_params)
     if @student.save
       flash[:notice] = "Student Successfully Saved"
-      redirect_to admissions_path
+      redirect_to students_path
     else
       render :new
       flash[:alert] = @student.errors.full_messages.to_sentence
     end
-  end
-
-  def update
-    @student = Student.find(params[:id])
-    @student.emergency_contacts.destroy_all
-    if @student.update_attributes(admission_params)
-      flash[:notice] = "Student Successfully Updated"
-      redirect_to admissions_path
-    else
-      render :new
-      flash[:alert] = @student.errors.full_messages.to_sentence
-    end
-  end
-
-  def destroy
-    @student = Student.find(params[:id])
-    @student.destroy
-    redirect_to admissions_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
   resources :students
+  post "students/remove_avatar", to: "students#remove_avatar"
+
   resources :subject_allocations
 
   get "visitors/contact_us", to: "visitors#contact_us"
   get "visitors/about_us", to: "visitors#about_us"
 
   resources :employees
+  post "employees/remove_avatar", to: "employees#remove_avatar"
+  
   resources :employee_positions
   resources :student_categories
   resources :departments
@@ -15,13 +19,13 @@ Rails.application.routes.draw do
   resources :subjects
   
   # student admission routes here
-  get "admission" => "admission#index", as: :admissions
+  # get "admission" => "admission#index", as: :admissions
   get "/admission/new" => "admission#new", as: :new_admission
   post "/admission" => "admission#create"
-  get "/admission/show/:id" => "admission#show", :as => :admission_detail
-  get "/admission/edit/:id" => "admission#edit", :as => :admission_edit
-  delete "/admission/destroy/:id" => "admission#destroy", :as => :delete_admission
-  post "/admission/update/:id" => "admission#update", as: :update_admission
+  # get "/admission/show/:id" => "admission#show", :as => :admission_detail
+  # get "/admission/edit/:id" => "admission#edit", :as => :admission_edit
+  # delete "/admission/destroy/:id" => "admission#destroy", :as => :delete_admission
+  # post "/admission/update/:id" => "admission#update", as: :update_admission
 
   # routes for school information
   get "general/settings", as: :school_edit
