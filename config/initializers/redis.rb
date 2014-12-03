f1db5e706623b602fea2cff001449e73c5059b7e
Ali Ahmed <ali_ahmed922@outlook.com>
2014-12-03 19:20:39 +0500

    redistogo url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+uri = URI.parse(ENV["postgres://fjmewerrlumgfa:qgriDFD-alwsxVFaigmV6cy-Ln@ec2-54-243-48-204.compute-1.amazonaws.com:5432/d19nqd35k4pti4
+"] || "redis://localhost:6379/" )
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 $redis = Redis.new(:host => 'localhost', :port => 6379)
