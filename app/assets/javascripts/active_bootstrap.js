59c37082dd97fb6fdc1881be8e765be01d2a123f
Ali Ahmed <ali_ahmed922@outlook.com>
2015-01-05 18:32:07 +0500

    profile hover

--- app/assets/javascripts/active_bootstrap.js
+++ app/assets/javascripts/active_bootstrap.js
@@ -8,14 +8,50 @@ $(function(){
 		// }
 		$('.batch_students').show()
 		$('.batch_subjects').hide()
-		$('.nav-pills li:first-child a').css("background-color", "transparent")
+		$('.nav-pills li:first-child a').css("background-color", "#0ce3ac")
+		$('.nav-pills li:last-child a').css("background-color", "transparent")
 		e.preventDefault();
 	});
 	$('.nav-pills li:last-child a').click(function(e) {
 		$('.batch_subjects').show()
 		$('.batch_students').hide()
 		$('.nav-pills li:first-child a').css("background-color", "transparent")
+		$('.nav-pills li:last-child a').css("background-color", "#0ce3ac")
 		e.preventDefault();
 	});
 	$('.nav-pills li:first-child a').css("background-color", "#0ce3ac")
+
+
+	// $("#subjects_students").hide()
+	// $('.nav_subjects li:last-child a').click(function(e) {
+	// 	$("#subjects_subjects").show()
+	// 	$("#subjects_students").hide()
+	// 	$('.nav_subjects li:first-child a').css("background-color", "transparent")
+	// 	$('.nav_subjects li:last-child a').css("background-color", "#0ce3ac")
+	// 	e.preventDefault();
+	// });
+
+	// $('.nav_subjects li:last-child a').click(function(e) {
+	// 	$("#subjects_subjects").hide()
+	// 	$("#subjects_students").show()
+	// 	$('.nav_subjects li:first-child a').css("background-color", "transparent")
+	// 	$('.nav_subjects li:last-child a').css("background-color", "#0ce3ac")
+	// 	e.preventDefault();
+	// });
+	// $('.nav_subjects li:first-child a').css("background-color", "#0ce3ac")
+
+});
+
+$(function(){
+$(".dropdown").hover(            
+        function() {
+            $('.student_profile_hover', this).stop( true, true ).fadeIn("fast");
+            $(this).toggleClass('open');
+            // $('b', this).toggleClass("caret caret-up");                
+        },
+        function() {
+            $('.student_profile_hover', this).stop( true, true ).fadeOut("fast");
+            $(this).toggleClass('open');
+            // $('b', this).toggleClass("caret caret-up");                
+        });
 });
