083cf2f91af6e55e4ef77882fab9a4f3318a023c
Ali Ahmed <ali_ahmed922@outlook.com>
2015-02-12 03:16:43 +0500

    time table

--- app/controllers/time_table_controller.rb
+++ app/controllers/time_table_controller.rb
@@ -1,5 +1,9 @@
 class TimeTableController < ApplicationController
 	def index
 		add_breadcrumb "Time Table"
+		@week_days = WeekDay.all
+		@class = SchoolClass.find_by_class_name(params[:search_class])
+		# @classes = SchoolClass.first.periods.includes(:subject).where("week_day_id = ? AND level = ?", week_day, n
+		@subjects = Subject.all
 	end
 end
